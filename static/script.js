13cc551f8c5289ae9355bea5805229ca20b55d87
baylagas <balbino_a@hotmail.com>
2021-02-16 20:05:47 -0600

    modify index and script, added hide div by id functionality

--- static/script.js
+++ static/script.js
@@ -1,2 +1,10 @@
-alert("hello world")
+function hideDivById(divId) {
+    var element = document.getElementById(divId)
+    if (element.style.display === "none") {
+        element.style.display = "block"
+    } else {
+        element.style.display = "none"
+    }
+}
+
 w3.includeHTML()
