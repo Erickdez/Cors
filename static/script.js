818829c5c7d4a4207d5d7d42333b99cc0d090a5a
baylagas <balbino_a@hotmail.com>
2021-02-16 20:16:57 -0600

    modify script have the correct functionality of links

--- static/script.js
+++ static/script.js
@@ -1,8 +1,14 @@
 function hideDivById(divId) {
+    hideAllDivW3Includes()
     var element = document.getElementById(divId)
     if (element.style.display === "none") {
         element.style.display = "block"
-    } else {
+    }
+}
+
+function hideAllDivW3Includes() {
+    var elementArray = document.getElementsByName("w3includes")
+    for (var element of elementArray) {
         element.style.display = "none"
     }
 }
